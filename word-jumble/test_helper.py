4de7d9f4a7d074ae4297cb2c381e9a12a670501d
maxdavila <adavila0711@gmail.com>
2014-01-29 11:09:58 -0800

    add test_helpers

---
+++ word-jumble/test_helper.py
@@ -0,0 +1,7 @@
+valid_dict = {'go': ['go'], 'do': ['do'], 'estt': ['test'], 'dgo': ['god'], 'ehllo': ['hello']}
+valid_perm = ['h', 'he', 'hel', 'hell', 'hello', 'e', 'el', 'ell', 'ello', 'l', 'll', 'llo', 'l', 'lo', 'o']
+valid_set = set(['go', 'do', 'god'])
+test_file_path = './test.txt'
+unknown_word = 'unknown'
+word = 'dog'
+hello = 'hello'
