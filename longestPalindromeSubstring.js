2e8318d8a3f2cc6f38135aedfb64f79a7a67ac22
maxdavila <adavila0711@gmail.com>
2014-02-26 12:16:43 -0800

    algorithms finds all palindromes

--- longestPalindromeSubstring.js
+++ longestPalindromeSubstring.js
@@ -1,5 +1,19 @@
 // Find the longest palindrome substring 
 
 function longestPalindromeSub(string) {
-	
+	var result = [];
+	for (var i = 0; i < string.length; i++) {
+		for (var j = 0 ; j < string.length; j++) {
+			var substr = string.substr(i, j+1)
+			if ((substr.length > 1) && (result.indexOf(substr) === -1) && isPalindrome(substr))
+				result.push(substr)
+		}
+	}
+	return result
 }
+
+function isPalindrome(string) {
+	return string.split('').reverse().join('') === string
+}
+
+console.log(longestPalindromeSub('aabaa'))
