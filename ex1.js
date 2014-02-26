89fb73e501632ccb79d831bf96cae227308e5264
maxdavila <adavila0711@gmail.com>
2014-02-25 22:16:41 -0800

    changed method into a recursive method. [this](http://codereview.stackexchange.com/questions/3222/finding-all-upper-lower-case-combinations-of-a-word) helped a lot.

--- ex1.js
+++ ex1.js
@@ -1,28 +1,22 @@
 // Given a lowercase string 'ab',write a program to generate 
 // all possible lowercase and uppercase combination 
-// {'AB',‘Ab’,'aB' and 'ab } 
+// {'AB',‘Ab’,'aB' and 'ab' } 
 
-function allCombination(str){
-	result = [str]
-	for (var i = 0; i < str.length; i++){
-		var strArray = str.split('');
-		result.push(generateLower(strArray.slice(0), i));
-		result.push(generateUpper(strArray, i));
-	}
-	result.push(str.toUpperCase())
-	return result;
+function allCombination(str, accu, idx, result){
+	if (idx === str.length) 
+		return result.push(accu.join(''));
+	var ch = str[idx];
+
+	accu[idx] = ch.toLowerCase();
+
+	allCombination(str, accu, idx+1, result);
+
+	accu[idx] = ch.toUpperCase();
+	allCombination(str, accu, idx+1, result);
+	return result
+
+
 }
 
-function generateUpper(str, idx) {
-	return str.map(function(el, index){
-		if (index === idx) return el;
-		return el.toUpperCase();
-	}).join('');
-}
 
-function generateLower(str, idx) {
-	str.splice(idx, 1, str[idx].toUpperCase());
-	return str.join('');
-}
-
-console.log(allCombination('abc'))
+console.log(allCombination('abcd', [], 0, []))
